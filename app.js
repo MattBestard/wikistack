78a2d08173ff44314213b9a45421e6c5d05d9352
hannahhopp <hannah.hopp@gmail.com>
2018-03-09 09:13:09 -0800

    trying to sync

--- app.js
+++ app.js
@@ -4,23 +4,29 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 const layout = require('./views/layout');
-const { db } = require('./models');
+const models = require('./models');
+const PORT = 1337;
 
-db.authenticate().
-then(() => {
-  console.log('connected to the database');
-})
+// models.authenticate()
+//   .then(() => {
+//   console.log('connected to the database');
+// });
+
+const init = async () => {
+  await models.User.sync();
+  await models.Page.sync();
+  app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+  });
+}
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
   // res.send("<h1>Hello World!</h1>");
   res.send(layout(''));
 });
 
-const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+
