a606407509490adeb1866cf6828d8f4edc0cb24b
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-03-09 11:53:06 -0500

    finished connecting to postgres

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+const layout = require('./views/layout');
+const { db } = require('./models');
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
 
 app.get('/', (req, res, next) => {
-  res.send("<h1>Hello World!</h1>");
+  // res.send("<h1>Hello World!</h1>");
+  res.send(layout(''));
 });
 
 const PORT = 1337;
